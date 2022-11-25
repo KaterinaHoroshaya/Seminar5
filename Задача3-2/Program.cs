688a9b9b3e792355648cb0e5a0e77539c7ccc923
Katerina <m-a-c-u-k-a@mail.ru>
2022-11-26 02:33:24 +0700

    Есть ли заданное число в массиве

---
+++ Задача3-2/Program.cs
@@ -0,0 +1,41 @@
+﻿int size = ReadInt("Введите размер массива: ");
+int[] numbers = new int[size];
+
+FillArrayRandomNumbers(numbers, 1, 10);
+WriteArray(numbers);
+
+int searchNumber = ReadInt("Введите искомое число: ");
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    if (searchNumber == numbers[i])
+    {
+        Console.WriteLine("Да");
+        return;
+    }
+}
+Console.WriteLine("Нет");
+
+void FillArrayRandomNumbers(int[] array, int min, int max)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
